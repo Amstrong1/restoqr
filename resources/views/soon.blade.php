ce578340490760d3cd71b01263906c36957a9d98
Amstrong1 <lewisamstrongh@gmail.com>
2024-05-09 11:59:40 +0100

    upd

---
+++ resources/views/soon.blade.php
@@ -0,0 +1,3 @@
+Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Error, accusantium. Reiciendis esse, eveniet et dolores, quibusdam eius eum incidunt quis, dignissimos aliquam earum beatae. 
+Maiores accusantium repellat quo eum minus!
