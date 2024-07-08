0cf0566cb9cb4f961b97c75822349f4c8ff9bd77
Amstrong1 <lewisamstrongh@gmail.com>
2024-07-08 09:41:57 +0100

    QR Code

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -6,7 +6,7 @@
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
                         <div class="p-6 text-gray-900">
-                            <h1>{{ __('message.edit') }}</h1>
+                            <h1>{{ __('Voir') }}</h1>
                             <x-forms.show :item="$order" :fields="$my_fields" type="order" />
                         </div>
                     </div>
--- resources/views/admin/place/show.blade.php
+++ resources/views/admin/place/show.blade.php
@@ -6,8 +6,34 @@
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
                         <div class="p-6 text-gray-900">
-                            <h1>{{ __('message.edit') }}</h1>
+                            <h1>{{ __('Voir') }}</h1>
                             <x-forms.show :item="$place" :fields="$my_fields" type="place" />
+                        </div>
+
+                        <div class="flex justify-between items-center p-6">
+                            <h2 class="font-semibold">QR CODE</h2>
+                            <a href="">
+                                <x-primary-button>
+                                    Imprimer 
+                                </x-primary-button>
+                            </a>
+
+                        </div>
+                        <div class="my-4 flex flex-col items-center">
+                            <div class="flex justify-center items-center">
+                                <div>{!! $qrCode !!}</div>
+                                <div>
+                                    <span
+                                        class="block transform rotate-90 text-2xl font-bold text-black relative -left-8">
+                                        CLUB NAME
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="text-gray-400 mb-8">
+                                <h2 class="mt-4 text-4xl font-bold text-orange-500 mb-2">SCAN FOR MENU</h2>
+                                Not working with your camera app? <br>
+                                Search for a free QR code reader on your app store.
+                            </div>
                         </div>
                     </div>
                 </div>
