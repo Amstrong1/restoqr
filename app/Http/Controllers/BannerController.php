0e60a9a70e657bfb44e3f0bdc1d2faa9aedb51c8
Amstrong1 <lewisamstrongh@gmail.com>
2024-09-21 17:21:23 +0100

    upd

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Banner;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class BannerController extends Controller
 {
     public function update(Request $request)
     {
-        $image = "";
+        $path = "";
         if ($request->image !== null) {
             $image = time() . '.' . $request->image->extension();
             $path = $request->file('image')->storeAs('banners', $image, 'public');
         }
 
         Banner::updateOrCreate(
-            ['structure_id' => auth()->user()->structure->id],
+            ['structure_id' => Auth::user()->structure->id],
             [
                 'title' => $request->title,
                 'image' => $path,
